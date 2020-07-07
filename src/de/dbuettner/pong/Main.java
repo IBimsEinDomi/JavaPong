c7741d3c8192a5dc6d67de70d709e5674debf65f
Dominik Büttner <50876016+IBimsEinDomi@users.noreply.github.com>
2020-07-07 14:18:19 +0200

    Added Sound Files

--- src/de/dbuettner/pong/Main.java
+++ src/de/dbuettner/pong/Main.java
@@ -1,8 +1,16 @@
 package de.dbuettner.pong;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		URL url = Gui.class.getResource("/start.wav");
+		AudioClip start = Applet.newAudioClip(url);
+		start.play();
 		
 		new Gui();
 		new Variables();
